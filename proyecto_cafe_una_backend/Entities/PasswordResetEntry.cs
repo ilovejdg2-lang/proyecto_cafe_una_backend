878b40b60db8ea9de6cfdc33767121a5b119ebc8
Maria del Mar Diaz Ruiz <maria.diaz.ruiz@est.una.ac.cr>
2026-06-05 15:05:44 -0600

    Se crea los services de usuario y autentificacion

--- proyecto_cafe_una_backend/Entities/PasswordResetEntry.cs
+++ proyecto_cafe_una_backend/Entities/PasswordResetEntry.cs
@@ -2,6 +2,7 @@ namespace proyecto_cafe_una_backend.Entities;
 
 public class PasswordResetEntry
 {
+    public int Id { get; set; }
     public string Token { get; set; } = string.Empty;
     public string Correo { get; set; } = string.Empty;
     public DateTime ExpiraEnUtc { get; set; }
